a502c8d5976259a336aa6ef61ee8481a13b06422
RobertSouDev <150874820+RobertSouDev@users.noreply.github.com>
2023-12-27 02:59:22 +0000

    modified pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>wedna, eu amo você .Se você me ama da ma risadinha </h1>
+    return <h1>wedna, eu amo você .Se você me ama da ma risadinha! 😎</h1>
 }
 
 export default Home;
